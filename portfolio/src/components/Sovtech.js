e292671ff6b3053ad3a0cbd80a7e6cbf7fdc55d8
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-11-20 21:47:49 +0200

    why sovtech section nearly done

---
+++ portfolio/src/components/Sovtech.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Sovtech() {
+  return (
+      <section id="lifestyle" className="text-black bg-white body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-black">
+            Why Sovtech?
+          </h1>
+          <div class="min-h-300 bg-gray-50 flex flex-col justify-center relative overflow-hidden sm:py-12">
+  <div class="max-w-7xl mx-auto">
+    <div class="relative group">
+      <div class="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+      <div class="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+        <div class="space-y-2">
+          <p class="text-slate-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+          <a href="https://braydoncoyer.dev/blog/tailwind-gradients-how-to-make-a-glowing-gradient-background" class="block text-indigo-400 group-hover:text-slate-800 transition duration-200" target="_blank">Read Article →</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+         
+          
+        </div>  
+        </div>
+
+ 
+
+
+
+        </section>
+
+  );
+}
