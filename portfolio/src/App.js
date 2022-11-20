d4f7e50183d55e8b16929e80161d4effa53465e0
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-11-20 18:52:05 +0200

    what I do section almost done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Life from "./components/Life";
 
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-white body-font">
+    <main className="text-black bg-white body-font">
       <Navbar />
       <About />
+      <Life/>
    
     </main>
   );
---
+++ portfolio/src/components/Life.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function Life() {
+  return (
+    <section id="projects" className="text-black bg-white body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-black">
+            Apps I've Built
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
+            fuga dolore.
+          </p>
+        </div>  
+        </div>
+
+ <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    {/* <!--Card 1--> */}
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-96" src="./me.jpg" alt="Mountain"/>
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Software Developer</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+      </div>
+    </div>
+    {/* card2 */}
+    <div class="rounded overflow-hidden shadow-lg">
+      <img class="w-96" src="./outing.jpeg" alt="River"/>
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Pageant Model</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#summer</span>
+      </div>
+    </div>
+    </div>
+
+
+
+        </section>
+        );
+}
