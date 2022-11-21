7fdefe256da2c9140c6e868dc6529af8f8d2be09
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-11-21 11:16:34 +0200

    fixed spelling matters

--- portfolio/src/components/Sovtech.js
+++ portfolio/src/components/Sovtech.js
@@ -15,23 +15,23 @@ export default function Sovtech() {
       <div class="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
         <div class="space-y-2">
           <p class="text-slate-800 text-lg">I hope to learn a lot from the SovTech internship.
-          I would love to be a developer at sovtech becuase the comapnys specs are aligned to my values in life.
-          I see an oportunity to grow and broaden my skills in the development industry through this internship.
-          I will contribute the skills that I have and still am about to acquire to the growth of the comapny as I am 
-          open to learning new skills everyday.
-        
-            <br></br>
-            <br></br>
-
-            I am excited about the possibilty of learning from and contributing to the team by applying my communication, teamwork
-            and decision-making skills. My focus is to utilize my skills in programming in order to make a 
-            positive impact on the overall organization. I enjoy being challenged , being a team player and
-            brainstorming project strategies. 
-
-            <br></br>
-            <br></br>
-            I believe these skills will be valuable in the graduate programme while 
-            continuing to enchance the excellent reputation of SovTech
+          I would love to be a developer at sovtech because the company's specs are aligned to my values in life.
+         I see an opportunity to grow and broaden my skills in the development industry through this internship.
+         I will contribute the skills that I have and still am about to acquire to the growth of the company as I am
+         open to learning new skills everyday.
+      
+           <br></br>
+           <br></br>
+ 
+           I am excited about the possibility of learning from and contributing to the team by applying my communication, teamwork
+           and decision-making skills. My focus is to utilize my skills in programming in order to make a
+           positive impact in the overall organization. I enjoy being challenged , being a team player and
+           brainstorming project strategies.
+ 
+           <br></br>
+           <br></br>
+           I believe these skills will be valuable in the graduate programme while
+           continuing to enhance the excellent reputation of SovTech
 
           </p>
           
