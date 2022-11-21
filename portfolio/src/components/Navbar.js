6d2f144cc23454ac85156467f770824aaf674472
nmotaung021 <nmotaung021@student.wethinkcode.co.za>
2022-11-21 05:59:10 +0200

    updated links and navbar

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -12,10 +12,16 @@ export default function Navbar() {
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
+            Projects
           </a>
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
+          </a>
+          <a href="#lifestyle" className="mr-5 hover:text-white">
+            Lifestyle
+          </a>
+          <a href="#sovtech" className="mr-5 hover:text-white">
+            Why SovTech
           </a>
         </nav>
         <a
